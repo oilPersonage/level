a2916f1319fbe49809a60fc8f52527dd704349b9
Масленников Андрей Вячеславович (4043523) <avmaslennikov@vtb.ru>
2023-08-23 12:33:28 +0300

    f

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,13 @@ import glsl from "vite-plugin-glsl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/level/",
   build: {
     sourcemap: true,
     // Reduce bloat from legacy polyfills.
     target: "esnext",
     // Leave minification up to applications.
     minify: false,
-    outDir: "./",
   },
   plugins: [
     glsl(),
